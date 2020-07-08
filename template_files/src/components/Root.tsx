63f25f1db94e7ae2f5e63995dd94af826075ef24
Grant Heath <heathgr@gmail.com>
2020-07-08 10:59:30 -0600

    Simplified package.json template.

--- template_files/src/components/Root.tsx
+++ template_files/src/components/Root.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react'
 
-const Root: FC = () => {
-  return (
-    <div>
-      <h1>You have successfully created an app. </h1>
-    </div>
-  )
-}
+const Root: FC = () => (
+  <div>
+    <h1>You have successfully created an app. </h1>
+  </div>
+)
 
 export default Root
