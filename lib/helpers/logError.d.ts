eee1a8f56dc201a7b6626cabd54e807119ef606b
Grant Heath <heathgr@gmail.com>
2020-08-01 15:14:31 -0600

    Reomve lib folder from repository.

--- lib/helpers/logError.d.ts
+++
@@ -1,7 +0,0 @@
-interface ExtendedError extends Error {
-    stdout?: string;
-    stderr?: string;
-}
-declare const logError: (e: ExtendedError) => void;
-export default logError;
-//# sourceMappingURL=logError.d.ts.map
